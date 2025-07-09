7f80ef8b95ee59684453e4529f36d8a61c2df1c6
Esteban <dannygghhm@gmail.com>
2025-07-09 17:46:34 -0400

    Adding temporary changes to Github

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,22 @@
-# from django.contrib import admin
-# from .models import related models
+# Uncomment the necessary imports at the top of the file
+from django.contrib import admin
+from .models import CarMake, CarModel  # Import your models
 
+# Define CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 1  # This determines how many empty inline forms to display initially
 
-# Register your models here.
+# Define CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'type', 'year', 'car_make']
 
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
+# Define CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]  # Add CarModelInline to CarMakeAdmin
+    list_display = ['name', 'description']  # Display fields for CarMake
+    search_fields = ['name']  # Enable search by name
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,2 +1,35 @@
+# Add this to server/djangoapp/populate.py
+
+from .models import CarMake, CarModel
+
 def initiate():
-    print("Populate not implemented. Add data manually")
+    # Sample data for CarMake
+    car_make_data = [
+        {"name": "NISSAN", "description": "Great cars. Japanese technology"},
+        {"name": "MAZDA", "description": "Great cars. German technology"},
+        {"name": "Hyundai", "description": "Great cars. Korean technology"},
+        {"name": "Toyota", "description": "Great cars. Japanese technology"},
+    ]
+    
+    # Create CarMake instances
+    car_make_instances = []
+    for data in car_make_data:
+        car_make_instances.append(CarMake.objects.create(name=data["name"], description=data["description"]))
+
+    # Sample data for CarModel with a relationship to CarMake
+    car_model_data = [
+        {"name": "Pathfinder", "type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
+        {"name": "Altima", "type": "Sedan", "year": 2023, "car_make": car_make_instances[0]},
+        {"name": "CX-5", "type": "SUV", "year": 2023, "car_make": car_make_instances[1]},
+        {"name": "Elantra", "type": "Sedan", "year": 2023, "car_make": car_make_instances[2]},
+        {"name": "Corolla", "type": "Sedan", "year": 2023, "car_make": car_make_instances[3]},
+    ]
+
+    # Create CarModel instances
+    for data in car_model_data:
+        CarModel.objects.create(
+            name=data["name"],
+            type=data["type"],
+            year=data["year"],
+            car_make=data["car_make"]
+        )
